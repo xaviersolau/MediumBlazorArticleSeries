e07410633b8cd49503b37e9ba5061c119ba5bbb6
Xavier Solau <xavier.solau@younited-credit.fr>
2020-12-21 14:18:57 +0100

    Add override culture comment.

--- ServerSideApp/Startup.cs
+++ ServerSideApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -38,7 +39,12 @@ namespace ServerSideApp
             {
                 options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
                 options.SupportedUICultures = supportedCultures;
+
+                //options.AddInitialRequestCultureProvider(
+                //    new CustomRequestCultureProvider(ctx => Task.FromResult(new ProviderCultureResult("en"))));
+
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
