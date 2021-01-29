b90d79ca03a2c5c105c6898651e516d53250aad9
Xavier Solau <xavier.solau@younited-credit.fr>
2021-01-29 19:42:33 +0100

    Clean JsonLocalizer project

---
+++ JsonLocalizer/JsonStringLocalizer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Localization;
+
+namespace JsonLocalizer
+{
+    public class JsonStringLocalizer : IStringLocalizer
+    {
+        private IFileProvider FileProvider { get; }
+        private string Name { get; }
+        private string ResourcesPath { get; }
+
+        public JsonStringLocalizer(IFileProvider fileProvider, string resourcePath, string name)
+        {
+            FileProvider = fileProvider;
+            Name = name;
+            ResourcesPath = resourcePath;
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                var stringMap = LoadStringMap();
+
+                return new LocalizedString(name, stringMap[name]);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var stringMap = LoadStringMap();
+
+                return new LocalizedString(name, string.Format(stringMap[name], arguments));
+            }
+        }
+
+        private Dictionary<string, string> LoadStringMap()
+        {
+            var cultureInfo = CultureInfo.CurrentUICulture;
+
+            var cultureName = cultureInfo.TwoLetterISOLanguageName;
+
+            var fileInfo =
+                FileProvider.GetFileInfo(
+                    Path.Combine(ResourcesPath, $"{Name}-{cultureName}.json"));
+
+            if (!fileInfo.Exists)
+            {
+                fileInfo =
+                    FileProvider.GetFileInfo(
+                        Path.Combine(ResourcesPath, $"{Name}.json"));
+            }
+
+            using var stream = fileInfo.CreateReadStream();
+
+            return JsonSerializer.DeserializeAsync<Dictionary<string, string>>(stream).Result;
+        }
+    }
+}
--- JsonLocalizer/JsonStringLocalizerFactory.cs
+++ JsonLocalizer/JsonStringLocalizerFactory.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Text.Json;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
@@ -20,82 +16,13 @@ namespace JsonLocalizer
 
         public IStringLocalizer Create(Type resourceSource)
         {
-            return new JsonStringLocalizer(new EmbeddedFileProvider(resourceSource.Assembly), ResourcesPath, resourceSource.Name);
+            var resources = new EmbeddedFileProvider(resourceSource.Assembly);
+            return new JsonStringLocalizer(resources, ResourcesPath, resourceSource.Name);
         }
 
         public IStringLocalizer Create(string baseName, string location)
         {
             throw new NotImplementedException();
-        }
-    }
-
-
-    public class JsonStringLocalizer : IStringLocalizer
-    {
-        private IFileProvider FileProvider { get; }
-        private string Name { get; }
-        private string ResourcesPath { get; }
-
-        public JsonStringLocalizer(IFileProvider fileProvider, string resourcePath, string name)
-        {
-            FileProvider = fileProvider;
-            Name = name;
-            ResourcesPath = resourcePath;
-        }
-
-        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IStringLocalizer WithCulture(CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-
-        public LocalizedString this[string name]
-        {
-            get
-            {
-                var stringMap = LoadStringMap();
-
-                return new LocalizedString(name, stringMap[name]);
-            }
-        }
-
-        public LocalizedString this[string name, params object[] arguments]
-        {
-            get
-            {
-                var stringMap = LoadStringMap();
-
-                return new LocalizedString(name, string.Format(stringMap[name], arguments));
-            }
-        }
-
-        private Dictionary<string, string> LoadStringMap()
-        {
-            var cultureInfo = CultureInfo.CurrentUICulture;
-
-            var cultureName = cultureInfo.TwoLetterISOLanguageName;
-
-            var fileInfo =
-                FileProvider.GetFileInfo(
-                    Path.Combine(ResourcesPath, $"{Name}-{cultureName}.json"));
-
-            if (!fileInfo.Exists)
-            {
-                fileInfo =
-                    FileProvider.GetFileInfo(
-                        Path.Combine(ResourcesPath, $"{Name}.json"));
-            }
-
-            using var stream = fileInfo.CreateReadStream();
-            using var buffer = new MemoryStream();
-
-            stream.CopyTo(buffer);
-
-            return JsonSerializer.Deserialize<Dictionary<string, string>>(buffer.ToArray());
         }
     }
 }
